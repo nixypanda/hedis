c9610b7938da5b242bfde6671a39ff4e548cb774
nixypanda <sherub.thakur@gmail.com>
2025-12-24 15:43:09 +0530

    Add RESP, handle ping and echo

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,89 @@
 
 module Main (main) where
 
+import Control.Monad (replicateM)
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.String (fromString)
 import Network.Simple.TCP (HostPreference (HostAny), Socket, closeSock, recv, send, serve)
 import System.IO (BufferMode (NoBuffering), hPutStrLn, hSetBuffering, stderr, stdout)
+import Text.Parsec (
+    ParseError,
+    anyChar,
+    char,
+    count,
+    crlf,
+    digit,
+    many1,
+    manyTill,
+    option,
+    parse,
+    (<|>),
+ )
+import Text.Parsec.ByteString (Parser)
+import Text.Parsec.Char (crlf)
+
+-- RESP
+
+data Resp = Str ByteString | Array Int [Resp] | BulkStr ByteString
+    deriving (Show)
+
+-- parsing
+
+bytes :: Parser ByteString
+bytes = fromString <$> manyTill anyChar (char '\r')
+
+resp :: Parser Resp
+resp = do
+    t <- anyChar
+    case t of
+        '+' -> Str <$> bytes <* crlf
+        '*' -> array
+        '$' -> bulkString
+        _ -> fail $ "invalid type tag: " ++ show t
+
+signedIntParser :: Parser Int
+signedIntParser = do
+    sign <- option '+' (char '+' <|> char '-')
+    digits <- many1 digit
+    let intValue = read digits :: Int
+    return $ if sign == '-' then -intValue else intValue
+
+array :: Parser Resp
+array = do
+    n <- signedIntParser <* crlf
+    if n >= 0
+        then Array (fromIntegral n) <$> replicateM n resp
+        else fail "negative array length"
+
+bulkString :: Parser Resp
+bulkString = do
+    n <- signedIntParser <* crlf
+    if n >= 0
+        then BulkStr . fromString <$> count n anyChar <* crlf
+        else fail "negative bulk length"
+
+decode :: ByteString -> Either ParseError Resp
+decode = parse resp ""
+
+crlf' :: ByteString
+crlf' = "\r\n"
+
+-- encoding
+
+encode :: Resp -> Either ByteString ByteString
+encode (Str x) = Right $ "+" <> x <> crlf'
+encode (BulkStr x) = Right $ "$" <> fromString (show $ BS.length x) <> crlf' <> x <> crlf'
+encode r = Left $ "Don't know how to encode this" <> fromString (show r)
+
+-- Command execution
+
+runCmd :: Resp -> Resp
+runCmd (Array 1 [BulkStr "PING"]) = Str "PONG"
+runCmd (Array 2 [BulkStr "ECHO", BulkStr xs]) = BulkStr xs
+
+-- network
 
 bufferSize :: Int
 bufferSize = 1024
@@ -12,12 +92,25 @@ bufferSize = 1024
 clientLoop :: Socket -> IO ()
 clientLoop socket = do
     redisCmdStr <- recv socket bufferSize
-    print redisCmdStr
     case redisCmdStr of
-        Nothing -> pure ()
+        Nothing -> do
+            print "Empty buffer"
+            pure ()
         Just redisCmdStr' -> do
-            send socket "+PONG\r\n"
-            clientLoop socket
+            print $ "String received: " <> redisCmdStr'
+            case decode redisCmdStr' of
+                Left err -> do
+                    print $ "RESP: Parsing error " <> show err
+                Right resp -> do
+                    print $ "RESP: Parsing Success" <> show resp
+                    let result = runCmd resp
+                    case encode result of
+                        Left err -> do
+                            print $ "RESP: Encoding error" <> err
+                        Right respStr -> do
+                            print $ "RESP: Encoding Success" <> respStr
+                            send socket respStr
+                            clientLoop socket
 
 main :: IO ()
 main = do
