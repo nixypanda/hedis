051077cf084191e5f25b8a3852e14ed0b736961c
nixypanda <sherub.thakur@gmail.com>
2025-12-26 19:21:38 +0530

    Handle autogeneration in xadd

--- src/StreamMap.hs
+++ src/StreamMap.hs
@@ -3,7 +3,8 @@ module StreamMap where
 import Data.List (unsnoc)
 import Data.Map (Map)
 import Data.Map qualified as M
-import Data.Time (UTCTime)
+import Data.Time (UTCTime, nominalDiffTimeToSeconds)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 
 data StreamId = AutoId | ExplicitId Int IdSeq
     deriving (Show, Eq)
@@ -32,19 +33,30 @@ type StreamMap k ik v = Map k [Value ik v]
 insert :: (Ord k) => k -> StreamId -> [(ik, v)] -> UTCTime -> StreamMap k ik v -> Either StreamMapError (ConcreteStreamId, StreamMap k ik v)
 insert key sid values time sMap
     | streamId == baseId = Left BaseStreamId
-    | Nothing <- M.lookup key sMap = okInsert []
-    | Just [] <- M.lookup key sMap = okInsert []
-    | Just xs <- M.lookup key sMap, Just (_, lastVal) <- unsnoc xs, lastVal.streamId >= streamId = Left NotLargerId
-    | Just xs <- M.lookup key sMap = okInsert xs
+    | Nothing <- mxs = okInsert [] streamId
+    | Just [] <- mxs = okInsert [] streamId
+    | Just xs <- mxs, Just (_, lastVal) <- unsnoc xs, lastVal.streamId >= streamId = Left NotLargerId
+    | Just xs <- mxs = okInsert xs streamId
   where
-    streamId = convertToConcreteStreamId sid time
-    okInsert xs =
+    mxs = M.lookup key sMap
+    oldSeq :: (Int, Int)
+    oldSeq =
+        case mxs >>= unsnoc of
+            Just (_, v) -> v.streamId
+            Nothing -> (0, 0)
+
+    streamId :: ConcreteStreamId
+    streamId =
+        case sid of
+            AutoId -> let ts = utcToMillis time in if ts == fst oldSeq then (ts, snd oldSeq + 1) else (ts, 0)
+            ExplicitId ts (Seq i) -> (ts, i)
+            ExplicitId ts SeqAuto -> if ts == fst oldSeq then (ts, snd oldSeq + 1) else (ts, 0)
+
+    okInsert xs sid' =
         Right
-            ( streamId
-            , M.insert key (xs ++ [MkValue streamId values]) sMap
+            ( sid'
+            , M.insert key (xs ++ [MkValue sid' values]) sMap
             )
 
-convertToConcreteStreamId :: StreamId -> UTCTime -> ConcreteStreamId
-convertToConcreteStreamId AutoId _ = undefined
-convertToConcreteStreamId (ExplicitId _ SeqAuto) _ = undefined
-convertToConcreteStreamId (ExplicitId ts (Seq i)) _ = (ts, i)
+utcToMillis :: UTCTime -> Int
+utcToMillis = floor . (* 1000) . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds
