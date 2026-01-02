a666177d2c424c669cf7579c5af5cd279637bd80
nixypanda <sherub.thakur@gmail.com>
2026-01-02 17:00:49 +0530

    Add haversine distance calculation

--- src/Geo/Types.hs
+++ src/Geo/Types.hs
@@ -26,3 +26,23 @@ maxLongitude = 180.0
 coordinatesAreValid :: Coordinates -> Bool
 coordinatesAreValid (MkCoordinates lat long) =
     minLatitude <= lat && lat <= maxLatitude && minLongitude <= long && long <= maxLongitude
+
+-- Redis uses ~6372797.560856!
+earthRadiusMeters :: Double
+earthRadiusMeters = 6372797.560856
+
+degToRad :: Double -> Double
+degToRad d = d * pi / 180
+
+haversineDistance :: Coordinates -> Coordinates -> Double
+haversineDistance (MkCoordinates lat1 lon1) (MkCoordinates lat2 lon2) =
+    earthRadiusMeters * c
+  where
+    φ1 = degToRad lat1
+    φ2 = degToRad lat2
+    φΔ = degToRad (lat2 - lat1)
+    λΔ = degToRad (lon2 - lon1)
+
+    a = sin (φΔ / 2) ^ (2 :: Int) + cos φ1 * cos φ2 * sin (λΔ / 2) ^ (2 :: Int)
+
+    c = 2 * atan2 (sqrt a) (sqrt (1 - a))
